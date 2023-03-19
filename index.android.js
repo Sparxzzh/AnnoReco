fe1aeece3dfce7945370fc790e0b17a4147bde6c
Spike <sparxzzh@gmail.com>
2023-03-19 16:23:12 +0800

    add some code

---
+++ index.android.js
@@ -0,0 +1,11 @@
+/**
+ * AnnoReco
+ * @flow
+ */
+
+import {
+    AppRegistry,
+} from 'react-native'
+import setup from './js/page/setup';
+
+AppRegistry.registerComponent('AnnoReco', () => setup);

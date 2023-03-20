5e86e11718aa2341f5e15439172794dc56721b79
Spike <sparxzzh@gmail.com>
2023-03-20 17:42:44 +0800

    migrate project

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
+import React from 'react';
 import {AppRegistry} from 'react-native';
+// import { AppearanceProvider } from 'react-native-appearance';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const AppWithAppearance = () => {
+  return (
+      <App/>
+  )
+}
+
+AppRegistry.registerComponent(appName, () => AppWithAppearance);
